e0ae291871001c3286bbbd607ac398ee4215c875
Pelayo Trives <pelayotrives@gmail.com>
2022-05-13 15:55:46 +0200

    Commit para Split con Josu.

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,4 +9,8 @@ router.get("/", (req, res, next) => {
 const authRouthes = require("./auth.routes.js")
 router.use("/auth", authRouthes)
 
+// Profile
+const profileRoutes = require("./profile.routes.js")
+router.use("/profile", profileRoutes)
+
 module.exports = router;
