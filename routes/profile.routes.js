a9038043dfcbcdcc9f14aeb0bf4ed86e14afc0d4
Pelayo Trives <pelayotrives@gmail.com>
2022-05-16 10:31:17 +0200

    Profile modificado.

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,44 +1,46 @@
-// const UserModel = require("../models/User.model.js");
-// const router = require("express").Router();
+const User = require("../models/User.model.js");
+const router = require("express").Router();
 
-// //GET: (/profile)=> Renderiza los datos de perfil del usuario
-// router.get("/", async (req, res, next) => {
-//   const {_id}=req.session.user
-//   try {
-//     let profile = await UserModel.findOne(_id);
-//     res.render("profile/profile-form.hbs", {
-//       profile,
-//     });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+//GET: (/profile)=> Renderiza los datos de perfil del usuario
+router.get("/", async (req, res, next) => {
+  const { user } = req.session;
+  console.log(user);
+  try {
+    let profile = await User.findById(user);
+    res.render("profile/profile-form.hbs", {
+      user,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
-// //POST: (/profile)=> Modifica los datos del usuario
-// router.post("/", async (req, res, next) => {
-//   const logUser = req.session.user;
-//   const { username, email } = req.body;
-//   try {
-//     let profile = await UserModel.findByIdAndUpdate(logUser, {
-//       username,
-//       email
-//     });
-//     res.render("profile/profile.hbs", {
-//       profile,
-//     });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+//POST: (/profile)=> Modifica los datos del usuario
+router.post("/", async (req, res, next) => {
+  const { user } = req.session;
+  const { username, email } = req.body;
+  try {
+    let profile = await User.findByIdAndUpdate(user, {
+      username,
+      email,
+    });
+    res.render("profile/profile.hbs", {
+      profile,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
-// //POST: (/profile/delete)=> elimina los datos del usuario
-// router.post("/delete", async (req, res, next) => {
-//   const logUser = req.session.user;
-//   try {
-//     let profile = await UserModel.findByIdAndDelete(logUser);
-//     res.redirect("/auth/signup");
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-// module.exports = router;
+//POST: (/profile/delete)=> elimina los datos del usuario
+router.post("/delete", async (req, res, next) => {
+  const { user } = req.session;
+  try {
+    let profile = await User.findByIdAndDelete(id);
+    res.redirect("/auth/signup");
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
