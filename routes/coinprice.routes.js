6afb752ab5217cb39470789322a1d3255eae734c
Pablo Orozco <78923863+pol1987@users.noreply.github.com>
2022-05-16 12:47:52 +0200

    coin list solved

--- routes/coinprice.routes.js
+++ routes/coinprice.routes.js
@@ -1,11 +1,47 @@
 const router = require("express").Router();
 const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
+
+
+
+// let func = async() => {
+//   let data = await CoinGeckoClient.ping()
+// }
+
+
 
 //*GET "/coinprice" => Renderizar la vista del listado de monedas.
 
-router.get("/", (req, res, next) => {
-    res.render("coins/coins-prices.hbs");
+// router.get("/", (req, res, next) => {
+//     res.render("coins/coins-prices.hbs");
+
+//   });
+
+
+router.get("/", async (req, res, next) => {
+    
+    try {
+      let marketCoin = await CoinGeckoClient.coins.markets()
+      res.render ("coins/coins-prices.hbs", {
+        marketCoin
+      })
+
+    } catch (err) {
+      next(err)
+    }
+    
+
   });
+
+
+
+
+
+    
+    
+ 
+
+
 
 
 
